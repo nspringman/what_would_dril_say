3c35417606213740e8299934835957dbe38c950b
Nathan Springman <naspringman@gmail.com>
2021-03-25 13:19:13 -0500

    semi-random placement of answer text box

--- draw.py
+++ draw.py
@@ -35,7 +35,7 @@ def introSlide(canvasWidth, canvasHeight, question):
     db.newPage(canvasWidth, canvasHeight)
     backgroundImage(canvasWidth, canvasHeight)
     backgroundSquares(canvasWidth,canvasHeight)
-    db.frameDuration(8)
+    db.frameDuration(2)
 
     db.fill(1,1,1)
     margin_bottom = 0.3 * canvasHeight
@@ -85,23 +85,27 @@ def answerSlide(canvasWidth, canvasHeight, answer):
     db.newPage(canvasWidth, canvasHeight)
     # backgroundSquares(canvasWidth,canvasHeight)
     backgroundImage(canvasWidth, canvasHeight)
-    db.frameDuration(8)
+    db.frameDuration(4)
 
     answer = "‘" + answer + "’"
 
     db.fill(1,1,1)
     margin_bottom = 0.4 * canvasHeight
-    margin_sides = 0.2 * canvasHeight
+    margin_sides = 0.15 * canvasHeight
+    box_width =  0.7 * canvasWidth
+    box_height = canvasHeight * 0.55
+    x_0 = 20 + ((canvasWidth - 40 - box_width) * random.random())
+    y_0 = 20 + ((canvasHeight * 0.85 - box_height) * random.random()) # canvasHeight * z gives top margin
     db.polygon(
-        (margin_sides, margin_bottom), 
-        (canvasWidth - margin_sides, margin_bottom),
-        (canvasWidth - margin_sides, canvasHeight - margin_sides),
-        (margin_sides, canvasHeight - margin_sides)
+        (x_0, y_0), 
+        (x_0 + box_width, y_0),
+        (x_0 + box_width, y_0 + box_height),
+        (x_0, y_0 + box_height)
     )
 
-    text_box_margin = margin_sides * 0.1
-    text_box_width = canvasWidth - margin_sides * 2 - text_box_margin * 2
-    text_box_height = canvasHeight - margin_sides - margin_bottom - text_box_margin * 2
+    text_box_margin = 20
+    text_box_width = box_width - text_box_margin
+    text_box_height = box_height - text_box_margin
     
     current_font_size = 10
     db.font('Georgia', current_font_size)
@@ -118,13 +122,12 @@ def answerSlide(canvasWidth, canvasHeight, answer):
             break
 
     db.fontSize(current_font_size)
-
     db.fill(0,0,0)
     db.textBox(
         answer,
         (
-            margin_sides + text_box_margin, 
-            margin_bottom + text_box_margin,
+            x_0 + text_box_margin, 
+            y_0 + text_box_margin,
             text_box_width,
             text_box_height
         ),
@@ -146,5 +149,6 @@ for q in data:
     output_path = 'output/post_' + str(q_num) + '.gif'
     db.saveImage(output_path)
     q_num += 1
+    break #for now, only do one post
 
 db.endDrawing()
